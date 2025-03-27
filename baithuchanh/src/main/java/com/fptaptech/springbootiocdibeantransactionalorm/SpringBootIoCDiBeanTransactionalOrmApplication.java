cace2ffb811d9e20cd6e2f8e062b557e68fc0112
nguyenhuuquan265 <142900574+nguyenhuuquan265@users.noreply.github.com>
2025-03-27 09:44:14 +0700

    Update SpringBootIoCDiBeanTransactionalOrmApplication.java

--- baithuchanh/src/main/java/com/fptaptech/springbootiocdibeantransactionalorm/SpringBootIoCDiBeanTransactionalOrmApplication.java
+++ baithuchanh/src/main/java/com/fptaptech/springbootiocdibeantransactionalorm/SpringBootIoCDiBeanTransactionalOrmApplication.java
@@ -1,13 +1,1 @@
-package com.fptaptech.springbootiocdibeantransactionalorm;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringBootIoCDiBeanTransactionalOrmApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootIoCDiBeanTransactionalOrmApplication.class, args);
-    }
-
-}
